0b050c30e1a781c136016d00f9ce7bdeda4aabc8
LiMinQianAI <129355409+LiMinQianAI@users.noreply.github.com>
2024-01-08 14:47:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,9 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "的朋友孟佳",  // 同上...
+        "送给我的",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "朋友孟佳",  // 同上...
         "今天是你的生日",
-        "生日快乐",
         "祝你在新的一岁里",
         "平平安安",
     ],
@@ -20,18 +19,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "Friendo": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "来首欢快的小曲",
+        bannar_coming: "赛博点灯",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        cake_fadein: "一块赛博蛋糕",
+        light_candle: "一根赛博蜡烛",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "COURAGEOUS",
     }
 };
