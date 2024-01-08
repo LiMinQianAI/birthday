f600375128ed6ef90882f679e3348d70cb11d9e5
LiMinQianAI <129355409+LiMinQianAI@users.noreply.github.com>
2024-01-08 15:19:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ var config = {
      */
     imgs: {
         "Friendo": "./imgs/flower.jpg",
-        "今天是你的生日": "./imgs/flower.jpg",
+         //"今天是你的生日": "./imgs/flower.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
